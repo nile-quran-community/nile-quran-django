277186862106e917bc24df00eec999d0412ee1f2
Youssef Fathy <32334265+0xzer0x@users.noreply.github.com>
2026-09-19 15:17:23 +0300

    feat(users): move role mapping to settings and add nqc teams

--- src/nile_quran_community_api/apps/v1/users/management/commands/setuproles.py
+++ src/nile_quran_community_api/apps/v1/users/management/commands/setuproles.py
@@ -1,51 +1,25 @@
+from django.conf import settings
 from django.contrib.auth.models import Group, Permission
 from django.core.management.base import BaseCommand
 
 
 class Command(BaseCommand):
     help = "Create user roles and assign permissions"
+    _BASE_INDENT = "  "
+
+    def _indent(self, level: int, text: str) -> str:
+        return (self._BASE_INDENT * level) + text
 
     def handle(self, *args, **kwargs) -> None:
         self.stdout.write(self.style.MIGRATE_HEADING("Setup roles:"))
-        students, _ = Group.objects.get_or_create(name="Student")
-        supervisors, _ = Group.objects.get_or_create(name="Supervisor")
-        admins, _ = Group.objects.get_or_create(name="Admin")
 
-        students.permissions.set(
-            [
-                Permission.objects.get(codename="view_user"),
-                Permission.objects.get(codename="view_activity"),
-                Permission.objects.get(codename="view_goal"),
-            ]
-        )
-        supervisors.permissions.set(
-            [
-                Permission.objects.get(codename="view_user"),
-                Permission.objects.get(codename="add_activity"),
-                Permission.objects.get(codename="view_activity"),
-                Permission.objects.get(codename="change_activity"),
-                Permission.objects.get(codename="delete_activity"),
-                Permission.objects.get(codename="view_goal"),
-            ]
-        )
-        admins.permissions.set(
-            [
-                Permission.objects.get(codename="add_user"),
-                Permission.objects.get(codename="view_user"),
-                Permission.objects.get(codename="change_user"),
-                Permission.objects.get(codename="delete_user"),
-                Permission.objects.get(codename="add_group"),
-                Permission.objects.get(codename="view_group"),
-                Permission.objects.get(codename="change_group"),
-                Permission.objects.get(codename="delete_group"),
-                Permission.objects.get(codename="add_activity"),
-                Permission.objects.get(codename="view_activity"),
-                Permission.objects.get(codename="change_activity"),
-                Permission.objects.get(codename="delete_activity"),
-                Permission.objects.get(codename="add_goal"),
-                Permission.objects.get(codename="view_goal"),
-                Permission.objects.get(codename="change_goal"),
-                Permission.objects.get(codename="delete_goal"),
-            ]
-        )
-        self.stdout.write("  Roles and permissions successfully set up.")
+        for name, perms in settings.GROUP_PERMISSIONS.items():
+            group, _ = Group.objects.get_or_create(name=name)
+            if len(perms) > 0:
+                group.permissions.set(Permission.objects.filter(codename__in=perms))
+
+                perm_names = group.permissions.values_list("name", flat=True)
+                self.stdout.write(self._indent(1, f"{name} permissions:"))
+                self.stdout.write(
+                    "\n".join(map(lambda p: self._indent(2, p), perm_names))
+                )
